ca8fbcf6535b5764ff991202050b15e9baf75166
Joao Marques <joao.marques@jogoglobal.com>
2026-06-24 13:48:30 -0300

    fix: pagina home completa

--- minatech/src/components/Navbar.jsx
+++ minatech/src/components/Navbar.jsx
@@ -1,64 +1,35 @@
-import "./Navbar.css"
-import logo from "../assets/img/logo.png"
-import { Link } from 'react-router-dom'
-import Programacao from '../pages/Programacao'
+import "./Navbar.css";
+import logo from "../assets/img/logo.png";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header>
-      <nav className="navbar navbar-expand-lg custom-navbar">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
-            <img src={logo} alt="MinaTech" id="logo" />
+    <header className="navbar-container">
+      <nav className="navbar-custom">
+        <div className="navbar-top">
+          <Link to="/" className="navbar-logo">
+            <img src={logo} alt="MinaTech" />
           </Link>
 
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
+            className="navbar-toggle"
+            onClick={() => setOpen(!open)}
+            aria-label="Abrir menu"
           >
-            <span className="navbar-toggler-icon"></span>
+            ☰
           </button>
+        </div>
 
-          <div
-            className="collapse navbar-collapse justify-content-end"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Quem somos
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <Link className="nav-link" to="/OSCS">
-                  OSCS
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Inscrição
-                </a>
-              </li>
-
-              <li className="nav-item">
-                <Link class="nav-link" to="/Programacao">Programação</Link>
-              </li>
-
-              <li className="nav-item">
-                <Link class="nav-link active" to="/FAQ">FAQ</Link>
-              </li>
-
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Contato
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className={`navbar-links ${open ? "open" : ""}`}>
+          <a href="#">Quem somos</a>
+          <Link to="/OSCS">OSCS</Link>
+          <Link to="/Inscricao">Inscrição</Link>
+          <Link to="/Programacao">Programação</Link>
+          <Link to="/FAQ">FAQ</Link>
+          <a href="#">Contato</a>
         </div>
       </nav>
     </header>
